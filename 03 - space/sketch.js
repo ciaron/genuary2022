63964bf5bf88d7c786253da84185d044452363da
Ciaron Linstead <linstead@pik-potsdam.de>
2022-01-05 18:30:12 +0100

    new sketch

--- 03 - space/sketch.js
+++ 03 - space/sketch.js
@@ -1,11 +1,12 @@
 // 03 Space
 
 let stars = [];
+let nstars = 1000;
 
 function setup() {
   createCanvas(1000, 1000);
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < nstars; i++) {
     let sz = map(i, 0, width, 0, PI);
     stars[i] = new Star(0,0);
   }
@@ -13,15 +14,15 @@ function setup() {
 
 function draw() {
   background(0);
-  push();
+  //push();
   translate(width/2, height/2);
   //console.log(stars);
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < nstars; i++) {
     stars[i].show();
     stars[i].update();
   }
-  pop();
+  //pop();
   //noLoop();
 
 }
@@ -35,12 +36,13 @@ class Star {
     // this.vel = createVector(1, -1);
     this.vel = p5.Vector.random2D();
     this.vel.mult(random(3));
+    this.acc = createVector(random(-0.01,0.01), random(-0.01, 0.01));
   }
 
   update() {
-    let mouse = createVector(mouseX, mouseY);
-    this.acc = p5.Vector.sub(mouse, this.pos);
-    this.acc.setMag(1);
+    //let mouse = createVector(mouseX, mouseY);
+    //this.acc = p5.Vector.sub(mouse, this.pos);
+    //this.acc.setMag(1);
 
     this.vel.add(this.acc);
     this.vel.limit(5);
@@ -49,9 +51,12 @@ class Star {
   }
 
   show() {
-    stroke(255);
-    strokeWeight(2);
-    fill(255, 100);
-    ellipse(this.pos.x, this.pos.y, 32);
+    //stroke(255);
+    //strokeWeight(2);
+    noStroke();
+    //let fl = map(this.pos.dist(createVector(0,0)), 0, width, 64, 255);
+    //fill(255, fl);
+    let sz = map(this.pos.dist(createVector(0,0)), 0, width, 0, 64);
+    ellipse(this.pos.x, this.pos.y, sz);
   }
 }
