2439f833b7f5702e8472ec7e50ed801c81e1b51b
Ciaron Linstead <linstead@pik-potsdam.de>
2022-01-06 17:08:13 +0100

    update 04; start 05,06,07,08

--- 04/sketch.js
+++ 04/sketch.js
@@ -1,28 +1,54 @@
 // 04 The next next Fidenza: https://tylerxhobbs.com/fidenza
 
+// With help from Coding Train - Perlin Noise Flowfield.
+
 let step=25;
 let noiseScale=0.02;
 let inc = 0.05;
+
+var cols, rows;
+var flowfield;
 
 function setup() {
   createCanvas(1000, 1000);
   //rectMode(CENTER);
   noStroke();
+  cols = floor(width / step);
+  rows = floor(height / step);
+
+  flowfield = new Array(cols * rows);
 }
 
 function draw() {
-
-  background(220);
+  background(255);
   
   let yoff=0;
 
-  for (y=0; y<height; y+=step){
+  for (y=0; y<rows; y++){
+
     let xoff=0; 
-    for (x=0; x<width; x+=step) {
+    for (x=0; x<cols; x++) {
+      var index = x + y*cols;
+      var angle = noise(xoff, yoff) * TWO_PI;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+
       //let f=random(255);
-      let f = noise(xoff, yoff);
-      fill(f*255);
-      rect(x,y,step,step);
+      //let f = noise(xoff, yoff);
+      //fill(f*255);
+      //rect(x,y,step,step);
+      
+      push();
+      translate(x*step, y*step);
+
+      //stroke(0);
+      //ellipse(0, 0, 2,2);
+
+      rotate(v.heading());
+      stroke(0, 50);
+      line(0,0,step,0);
+      pop();
       xoff += inc;
     }
     yoff += inc;
