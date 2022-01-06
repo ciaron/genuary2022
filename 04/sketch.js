dde9f62c0a3a533aeb6a19657092081cd476a44f
Ciaron Linstead <ciaron.linstead@gmail.com>
2022-01-06 20:19:58 +0100

    add curves to flowfield

--- 04/sketch.js
+++ 04/sketch.js
@@ -2,9 +2,9 @@
 
 // With help from Coding Train - Perlin Noise Flowfield.
 
-let step=25;
-let noiseScale=0.02;
-let inc = 0.05;
+let step=10;
+//let noiseScale=0.02;
+let inc = 0.025;
 
 var cols, rows;
 var flowfield;
@@ -22,6 +22,8 @@ function setup() {
 function draw() {
   background(255);
   
+  // set up and draw the flowfield
+
   let yoff=0;
 
   for (y=0; y<rows; y++){
@@ -29,7 +31,7 @@ function draw() {
     let xoff=0; 
     for (x=0; x<cols; x++) {
       var index = x + y*cols;
-      var angle = noise(xoff, yoff) * TWO_PI;
+      var angle = 2*(1-noise(xoff, yoff)) * TWO_PI;
       var v = p5.Vector.fromAngle(angle);
       v.setMag(1);
       flowfield[index] = v;
@@ -54,5 +56,57 @@ function draw() {
     yoff += inc;
   }
 
+  drawCurve(500,100);
+  drawCurve(250,750);
+  drawCurve(50,640);
   noLoop();
+
 }
+
+function drawCurve(x, y) {
+    // draw curves through the flowfield, as https://tylerxhobbs.com/essays/2020/flow-fields
+
+    // starting point
+    //var x = 500;
+    //var y = 100;
+    var num_steps=100;
+
+    var left_x = 0;
+    var top_y = 0;
+
+    stroke(255,0,0);
+    noFill();
+
+    beginShape();
+
+    for (let n=0; n<num_steps; n++) {
+
+      curveVertex(x, y);
+      ellipse(x,y,4,4);
+
+      x_offset = x - left_x;
+      y_offset = y - top_y;
+
+      var col = int(x_offset / step);
+      var row = int(y_offset / step);
+      var index = col + row*cols;
+
+      // do bounds checking here
+      if (col < cols && row < rows && col >=0 && row >= 0) {
+        //console.log(x_offset, y_offset, col, row, index);
+        var grid_angle = flowfield[index].heading();
+
+        var x_step = step * cos(grid_angle);
+        var y_step = step * sin(grid_angle);
+
+        //console.log(x_step, y_step);
+
+        x = x + x_step;
+        y = y + y_step;
+      }
+    }
+
+    endShape();
+  }
+  
+
